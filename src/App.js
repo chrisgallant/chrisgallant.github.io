0a261bc71d2ee21d89a96dda2ef8a6bc20061e83
Chris Gallant <christopherjgallant@gmail.com>
2021-12-27 14:57:48 -0600

    react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import manPoursSomeWater from './art/man-pours-some-water.png';
+
+function App() {
+  return (
+    <div>
+      <h2>chrisgallant.com</h2>
+        <img src={manPoursSomeWater} alt="man pours some water"/>
+    </div>
+  );
+}
+
+export default App;
