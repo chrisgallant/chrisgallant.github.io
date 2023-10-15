33d2c195974a80a7d20d1e899c72ace4b6673b13
Chris Gallant <christopherjgallant@gmail.com>
2023-10-15 17:19:52 -0500

    using hash router instead of browserrouter for gh pages

--- src/components/thumbnail.js
+++ src/components/thumbnail.js
@@ -1,7 +1,7 @@
 import { Badge, Box, Image } from "@chakra-ui/react";
 export const Thumbnail = ({ src, alt, title, year, media, size, isSold }) => {
   return (
-    <Box as="a" href={`/${title}`} maxW="sm" borderWidth="1px" borderRadius="md" overflow="hidden">
+    <Box as="a" href={`/#/${title}`} maxW="sm" borderWidth="1px" borderRadius="md" overflow="hidden">
       <Image src={src} alt={alt} height='200px' objectFit='cover' width='100%'style={{transform: 'translate3d(0, 0, 1px)'}}/>
 
       <Box p="6">
